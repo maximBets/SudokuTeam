217b617e09f4363bd3d1fb73609638501cb22fbf
Maxim <maxbeat040@gmail.com>
2023-03-25 12:43:28 +0300

    добавлена в функцию solve рендер игрового поля

--- sudoku.js
+++ sudoku.js
@@ -1,27 +1,34 @@
+const fs = require('fs');
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
+const boardString = fs.readFileSync('./puzzles.txt', 'utf-8').split('\n')[0];
+
 function solve(boardString) {
+  const arrBoard = boardString.split('');
+  const arraysize = 9;
+  const slicedarray = [];
+  for (let i = 0; i < arrBoard.length; i += arraysize) {
+    slicedarray.push(arrBoard.slice(i, i + arraysize));
+  }
+  return slicedarray;
 }
+console.log(solve(boardString));
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {

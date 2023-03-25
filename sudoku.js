7413ebef6247b845d683a284d5debcfde2de29aa
Штас <stanislav.sinitsyn.work@gmail.com>
2023-03-25 17:33:08 +0300

    изменение в функции solve

--- sudoku.js
+++ sudoku.js
@@ -1,34 +1,77 @@
-const fs = require('fs');
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-const boardString = fs.readFileSync('./puzzles.txt', 'utf-8').split('\n')[0];
+const fs = require('fs')
+const path = require('path')
 
-function solve(boardString) {
-  const arrBoard = boardString.split('');
-  const arraysize = 9;
-  const slicedarray = [];
-  for (let i = 0; i < arrBoard.length; i += arraysize) {
-    slicedarray.push(arrBoard.slice(i, i + arraysize));
-  }
-  return slicedarray;
+
+function solve() {
+
+const indexArgv = Number(process.argv[2]) || 0
+const stringSudoku = fs.readFileSync(path.join(__dirname, 'puzzles.txt'), 'utf-8').split('\n')[indexArgv]
+
+const arrSudoku = []
+const arrSudoku2 = []
+
+for (let i = 0; i < stringSudoku.length; i += 9) {
+  arrSudoku.push(stringSudoku.slice(i, i + 9).split(''))
+  arrSudoku2.push(stringSudoku.slice(i, i + 9).split(''))
 }
-console.log(solve(boardString));
+
+const fillNumArrSudoku = arrSudoku.map((el) => {
+   const existingDigits = el.filter(val => val !== '-').map(val => Number(val))
+   const missingDigits = Array.from({length: 9}, (_, i) => i + 1).filter(num => !existingDigits.includes(num))
+    for (let i = 0; i < el.length; i++) {
+     if (el[i] === '-') {
+       const randomNumber = Math.floor(Math.random() * missingDigits.length)
+       el[i] = missingDigits[randomNumber].toString()
+      //  missingDigits.splice(randomNumber, 1)
+     }
+   }
+   return el
+})
+
+const transposedMatrix = arrSudoku2[0].map((col, c) => arrSudoku2.map((row, r) => arrSudoku2[r][c]))
+
+const fillNumTransposedArrSudoku = transposedMatrix.map((el) => {
+      const existingDigits = el.filter(val => val !== '-').map(val => Number(val))
+      const missingDigits = Array.from({length: 9}, (_, i) => i + 1).filter(num => !existingDigits.includes(num))
+       for (let i = 0; i < el.length; i++) {
+        if (el[i] === '-') {
+          const randomNumber = Math.floor(Math.random() * missingDigits.length)
+          el[i] = missingDigits[randomNumber].toString()
+         //  missingDigits.splice(randomNumber, 1)
+        }
+      }
+      return el
+   })
+
+let final = fillNumArrSudoku.join('\n').replaceAll(',', ' ')
+let finalTranspose = fillNumTransposedArrSudoku.join('\n').replaceAll(',', ' ')
+
+console.log(final);
+}
+
+solve()
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {}
+function isSolved(board) {
+
+}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {}
+function prettyBoard(board) {
+
+}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {

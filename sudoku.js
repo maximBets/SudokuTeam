a32f1c811206112f448b21a322962866937a6960
Maxim <maxbeat040@gmail.com>
2023-03-25 17:49:59 +0300

    prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const { EOL } = require('os');
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-const boardString = fs.readFileSync('./puzzles.txt', 'utf-8').split('\n')[0];
+const boardString = fs.readFileSync('./puzzles.txt', 'utf-8').split(EOL)[0];
 
 function solve(boardString) {
   const arrBoard = boardString.split('');
@@ -28,7 +29,31 @@ function isSolved(board) {}
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {}
+
+// const board = ['1', '2', '5', '8', '1', '2', '2', '4', '5']
+
+const board = solve(boardString);
+
+function prettyBoard(board) {
+  let counter = -1;
+  let resArr = [];
+
+  for (let i = 0; i < board.length; i += 1) {
+    for (let j = 0; j < board.length; j++) {
+      counter++;
+      if (counter === 3) {
+        counter = 0;
+        resArr.push('|');
+      }
+      resArr.push(board[i]);
+    }
+  }
+  console.log(resArr);
+  const gameBoard = board.join('\n').replaceAll(',', ' ');
+
+  return gameBoard;
+}
+console.table(prettyBoard(board));
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {

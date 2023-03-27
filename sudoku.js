5525882e8a6594d4dffbe830dd9f0cb79a5f8331
Maxim <maxbeat040@gmail.com>
2023-03-27 11:02:05 +0300

    final prettyBoard

--- sudoku.js
+++ sudoku.js
@@ -20,7 +20,6 @@ function solve() {
     arrSudoku.push(stringSudoku.slice(i, i + 9).split(''));
     arrSudoku2.push(stringSudoku.slice(i, i + 9).split(''));
   }
-  const boardString = fs.readFileSync('./puzzles.txt', 'utf-8').split(EOL)[0];
 
   const fillNumArrSudoku = arrSudoku.map((el) => {
     const existingDigits = el
@@ -33,7 +32,6 @@ function solve() {
       if (el[i] === '-') {
         const randomNumber = Math.floor(Math.random() * missingDigits.length);
         el[i] = missingDigits[randomNumber].toString();
-        //  missingDigits.splice(randomNumber, 1)
       }
     }
     return el;
@@ -54,21 +52,12 @@ function solve() {
       if (el[i] === '-') {
         const randomNumber = Math.floor(Math.random() * missingDigits.length);
         el[i] = missingDigits[randomNumber].toString();
-        //  missingDigits.splice(randomNumber, 1)
       }
     }
     return el;
   });
   return fillNumTransposedArrSudoku;
-  // let final = fillNumArrSudoku.join('\n').replaceAll(',', ' ');
-  // let finalTranspose = fillNumTransposedArrSudoku
-  //   .join('\n')
-  //   .replaceAll(',', ' ');
-
-  // console.log(final);
 }
-
-// console.log(solve());
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
@@ -90,13 +79,10 @@ function prettyBoard(board) {
   let newArr = [];
 
   for (let i = 0; i < board.length; i += 1) {
-    let counter = -1;
     let resArr = [];
     newArr.push(resArr);
     for (let j = 0; j < board.length; j += 1) {
-      counter++;
-      if (counter === 3) {
-        counter = 0;
+      if (j % 3 === 0 && j !== 0) {
         resArr.push('|');
       }
       resArr.push(board[j][i]);
@@ -110,9 +96,7 @@ function prettyBoard(board) {
     }
     resArrFinal.push(newArr[i]);
   }
-  // console.log(resArr);
-  const gameBoard = resArrFinal.join('\n').replaceAll(',', ' ');
-  return gameBoard;
+  return resArrFinal.join('\n').replaceAll(',', ' ');
 }
 console.log(prettyBoard(solve()));
 
